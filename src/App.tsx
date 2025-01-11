a9d9d3c7ca0130734d46ccffc11610d1d9ab3951
Hristaka23 <ivanhristov1029@gmail.com>
2025-01-11 15:01:55 +0200

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, {useState, useEffect} from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import styles from '../src/assets/css/App.module.css'
 import Home from '../src/pages/Home'
 import Standings from '../src/pages/Standings'
+import {Analytics} from "@vercel/analytics/react"
+
 function App() {
 
-  return (
-    <>
-     
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/teams" element={<Standings/>} />
-        <Route path="/admin" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
-    </>
-  )
+    return (
+        <>
+            <Analytics/>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/teams" element={<Standings/>}/>
+                    <Route path="/admin" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
